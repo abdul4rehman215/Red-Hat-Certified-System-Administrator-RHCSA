a8b79b8fefc5b42cf3c1383467f1de0ac07cee5b
Abdul Rehman <abdul4rehman215@gmail.com>
2026-02-23 13:13:52 +0530

    Create commands.sh

---
+++ lab13-conditionally-execute-code-in-shell-scripts/commands.sh
@@ -0,0 +1,39 @@
+# Lab 13 - Conditionally Execute Code in Shell Scripts
+# Environment: Ubuntu 24.04.1 LTS
+
+# Verify environment
+cat /etc/os-release
+
+# Create basic_if.sh
+nano basic_if.sh
+chmod +x basic_if.sh
+./basic_if.sh
+
+# Create if_elif_else.sh
+nano if_elif_else.sh
+chmod +x if_elif_else.sh
+./if_elif_else.sh
+./if_elif_else.sh
+./if_elif_else.sh
+
+# Create file_check.sh
+nano file_check.sh
+chmod +x file_check.sh
+./file_check.sh
+
+# Create string_compare.sh
+nano string_compare.sh
+chmod +x string_compare.sh
+./string_compare.sh
+./string_compare.sh
+./string_compare.sh
+
+# Create test_command.sh
+nano test_command.sh
+chmod +x test_command.sh
+./test_command.sh
+./test_command.sh
+
+# Modify test_command.sh to use [ ] syntax
+nano test_command.sh
+./test_command.sh
